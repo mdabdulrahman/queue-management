d6a2b2f5d4b7fac4958c0a203578242d90a42207
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-09-18 14:27:00 +0530

    sol

--- src/Components/landingPage/Intro.js
+++ src/Components/landingPage/Intro.js
@@ -4,10 +4,10 @@ function Intro() {
   return (
     <div className='h-screen'>
 <img src={intro} className="w-screen absolute h-[90%] blur-sm object-cover brightness-75"></img>
-  <div className="absolute w-full flex justify-center items-center h-full">
+  <div className="inner-intro">
  <span >
   <h1 className='intro-text text-center text-5xl animate__animated animate__backInDown'>No More Standing In Queues </h1>
-<h1 className='text-center py-3 intro-text text-2xl'>Manage Queues in Your Shop Online</h1>
+<h1 className='text-center py-3 intro-text-2 '>Manage Queues in Your Shop Online</h1>
  <button className='btn-secondary   shadow mx-auto'>Get Started</button>
  </span>
    </div>
