bcf0fb06d07f78062d18d7359f8e2f8a9f407dce
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-09-19 21:49:01 +0530

    sol

--- src/Components/NewAccount/Form.js
+++ src/Components/NewAccount/Form.js
@@ -67,7 +67,12 @@ e.target.classList.add("greenBorder")
 <h1 className='text-sm text-center'>To Manage Queue in your Shop</h1>
 </legend>
 <div className='inner-form'>
-    <label >Buisness Name</label>
+    <label className='icontext'>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+</svg>
+
+Buisness Name</label>
     <input type="text" name="bname" ref={props.bname}  className="form-control" onChange={borderChange} required></input>
     <label>Buisness Type</label>
 <input type="text" className="form-control" ref={props.btype} name="btype" onChange={borderChange} ></input>
--- src/Components/signIn/SignIn.js
+++ src/Components/signIn/SignIn.js
@@ -17,13 +17,27 @@ export default class SignIn extends Component {
   render() {
     return (
       <div>
-        <fieldset className='form'>
-            <legend>Sign In</legend>
-            <label>Email</label>
+        <fieldset className='form-sign'>
+   
+            <label className='flex gap-2 items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
+</svg>
+
+              Email</label>
             <input type="email" className='form-control' onChange={(e)=>{this.setState({email:e.target.value})}} name="email"></input>
-        <label>Password</label>
+        <label className='flex gap-2 items-center'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
+</svg>
+Password</label>
         <input type="password" className='form-control' onChange={(e)=>{this.setState({password:e.target.value})}} name="password"></input>
-        <button className='btn-secondary m-2' onClick={()=>this.props.signin(this.state.email,this.state.password)}>Sign In</button>
+        <button className='btn-secondary m-2 ' onClick={()=>this.props.signin(this.state.email,this.state.password)}>Sign In</button>
+       <span className='col-span-2 text-sm text-center'>Don't have account ? <a href="/NewAccount">Register Now</a></span>
+       <span className=' text-sm flex justify-center items-center gap-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+</svg>
+Forgot Password </span>
         </fieldset>
        
       </div>
