4df6e407d4b41edd20c9d5ddf0c73297fea916c5
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-09-19 18:42:35 +0530

    sol

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -22,11 +22,13 @@ else if(props.btn==""){
   
 }
 
+if (mobile>=600){
+  
 
   return (
     <div className='header'>
    <div className='wrap flex items-center gap-2 font-black'>
-    <img className='secondary-bg rounded-full p-1 w-11' src={logo}></img>
+    <img className=' rounded-full p-1 w-11' src={logo}></img>
     Queue System
    </div>
    <div className='flex'>
@@ -40,7 +42,31 @@ else if(props.btn==""){
    </div>
     </div>
     </div>
-  )
+  )}
+  else{
+    return(
+      <div className='header'>
+        <div className='wrap'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+</svg>
+</div>
+      <div className='wrap flex items-center gap-2 font-black'>
+       <img className=' rounded-full p-1 w-11' src={logo}></img>
+  
+      </div>
+      <div className='flex'>
+      <div className='wrap'>
+       {btn1}
+      
+      </div>
+      <div className='wrap'>
+       {btn2}
+      
+      </div>
+       </div>
+       </div>
+    )
+  }
 }
 
 export default Header
--- src/Components/landingPage/Intro.js
+++ src/Components/landingPage/Intro.js
@@ -3,7 +3,7 @@ import intro from "./into.jpg"
 function Intro() {
   return (
     <div className='h-screen'>
-<img src={intro} className="w-screen absolute h-[90%] blur-sm object-cover brightness-75"></img>
+<img src={intro} className="w-screen absolute h-[90%] blur-sm object-cover brightness-50 "></img>
   <div className="inner-intro">
  <span >
   <h1 className='intro-text text-center text-5xl animate__animated animate__backInDown'>No More Standing In Queues </h1>
